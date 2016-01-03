dcbcee9344fccf80f8d459476c6cb48bd73fe90a
j129008 <j129008@gmail.com>
2016-01-03 21:30:46 +0800

    dataGen correct

--- dataGen.py
+++ dataGen.py
@@ -1,7 +1,7 @@
 import json
 import jieba
 import jieba.analyse
-import operator
+# import operator
 
 jieba.analyse.set_stop_words("./stopWords.txt")
 jieba.load_userdict("./pttDict.txt")
@@ -10,33 +10,63 @@ with open('./data.json') as data_file:
     data = json.load(data_file)
 
 instance = {}
+genData = []
 for post in data:
     instance['5F'] = 0
-    instance['肥宅'] = 0
-    instance['魯宅'] = 0
-    instance['本魯'] = 0
-    instance['小魯'] = 0
-    instance['魯蛇'] = 0
-    instance['女生'] = 0
-    instance['分手'] = 0
-    instance['正妹'] = 0
-    instance['女朋友'] = 0
-    instance['老婆'] = 0
-    instance['女人'] = 0
-    instance['女友'] = 0
-    instance['妹妹'] = 0
-    instance['馬習'] = 0
-    instance['總統'] = 0
-    instance['馬英九'] = 0
-    instance['國民黨'] = 0
-    instance['中國'] = 0
-    instance['媒體'] = 0
-    instance['記者'] = 0
+    instance['loser'] = 0
+    instance['loser_self'] = 0
+    instance['girl'] = 0
+    instance['beauty'] = 0
+    instance['sister'] = 0
+    instance['taiwan'] = 0
+    instance['reporter'] = 0
+    instance['news'] = 0
 
     for push in post['g_推文']:
         if "五楼" in post['g_推文'][push]['留言內容']:
             instance['5F'] = 1
 
     content = post['f_內文']
+    if "鲁蛇" in content:
+        instance['loser'] = 1
+    if "鲁宅" in content:
+        instance['loser'] = 1
     if "肥宅" in content:
-        instance['肥宅'] = 1
+        instance['loser'] = 1
+
+    if "本鲁" in content:
+        instance['loser_self'] = 1
+    if "小鲁" in content:
+        instance['loser_self'] = 1
+
+    if "女生" in content:
+        instance['girl'] = 1
+    if "分手" in content:
+        instance['girl'] = 1
+    if "老婆" in content:
+        instance['girl'] = 1
+    if "女人" in content:
+        instance['girl'] = 1
+    if "女友" in content:
+        instance['girl'] = 1
+    if "妹妹" in content:
+        instance['sister'] = 1
+    if "正妹" in content:
+        instance['beauty'] = 1
+    if "马习"  in content:
+        instance['taiwan'] = 1
+    if  "总统" in content:
+        instance['taiwan'] = 1
+    if  "马英九"  in content:
+        instance['taiwan'] = 1
+    if  "国民党" in content:
+        instance['taiwan'] = 1
+    if  "中国" in content:
+        instance['taiwan'] = 1
+    if "媒体" in content:
+        instance['news'] = 1
+    if "记者" in content:
+        instance['reporter'] = 1
+    # genData.extend(instance)
+    print(instance)
+
