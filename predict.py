2fdf7b56dad9c30db2b4e346a687b33728cc994e
j129008 <j129008@gmail.com>
2016-01-08 01:15:12 +0800

    to function

--- predict.py
+++ predict.py
@@ -1,4 +1,3 @@
-import sys
 import jieba
 import jieba.analyse
 import numpy as np
@@ -8,42 +7,39 @@ import opencc
 jieba.analyse.set_stop_words("./stopWords.txt")
 jieba.load_userdict("./pttDict.txt")
 
-instance = {}
-genData = []
-x = []
-y = []
+def predict(content):
+    instance = {}
+    x = []
 
-featureList = []
+    featureList = []
 
-with open('feature.pkl', 'rb') as f0:
-    featureList = pickle.load(f0)
+    with open('feature.pkl', 'rb') as f0:
+        featureList = pickle.load(f0)
 
-for ele in featureList:
-    instance[ele] = 0
+    for ele in featureList:
+        instance[ele] = 0
 
-content = sys.argv[1]
-content = opencc.convert(content)
+    content = opencc.convert(content)
 
-print(content)
-for ele in featureList:
-    if ele in content:
-        instance[ele] = 1
+    for ele in featureList:
+        if ele in content:
+            instance[ele] = 1
 
-term = []
-x = []
-for ele in featureList:
-    term.append(instance[ele])
-x.append(term)
+    term = []
+    x = []
+    for ele in featureList:
+        term.append(instance[ele])
+    x.append(term)
 
-X = np.array(x)
+    X = np.array(x)
 
-with open('lsa.pkl', 'rb') as f1:
-    lsa = pickle.load(f1)
-X = lsa.transform(X)
+    with open('lsa.pkl', 'rb') as f1:
+        lsa = pickle.load(f1)
+    X = lsa.transform(X)
 
-with open('model.pkl', 'rb') as f2:
-    clf = pickle.load(f2)
+    with open('model.pkl', 'rb') as f2:
+        clf = pickle.load(f2)
 
-yPred = clf.predict(X)
-print(yPred)
+    yPred = clf.predict(X)
+    return yPred[0]
 
