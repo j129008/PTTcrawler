51bd921f9065bd5ed82646d7b3117450700e3396
j129008 <j129008@gmail.com>
2016-01-13 18:25:11 +0800

    web.py

--- web.py
+++ web.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import request
 from  predict import predict
+from flask import render_template
 
 app = Flask(__name__)
-@app.route("/dm", methods=['GET'])
-def index():
-    return str(predict( request.args['keyword'] ))
+
+@app.route("/")
+def indexGet():
+    if 'keyword' in request.args:
+        result = predict( request.args['keyword'] )
+        if result == 1:
+            return render_template('app.html', foo='成功！')
+    return render_template('app.html', foo='失敗')
+
 
 if __name__ == "__main__":
     app.debug = True
